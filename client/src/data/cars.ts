138661c5b233315eaacf5da0829d8bb0d8747c0d
Saweed07 <saweed585@yahoo.com>
2026-06-17 22:09:57 +0530

    cars

--- client/src/data/cars.ts
+++ client/src/data/cars.ts
@@ -3,9 +3,9 @@ import { type Car } from "@shared/schema";
 export const carsData: Car[] = [
  {
     id: 1,
-    name: "Toyota Fortuner",
+    name: "Toyota Fortuner 4WD",
     type: "SUV",
-    pricePerDay: "₹11,999",
+    pricePerDay: "₹12,999",
     originalPricePerDay: "₹14,999",
     discountTag: "Luxury",
     imageUrl: "/Images/fortuner.avif"
@@ -30,6 +30,15 @@ export const carsData: Car[] = [
   },
   {
     id: 4,
+    name: "Toyota Hilux 4WD",
+    type: "SUV",
+    pricePerDay: "₹12,999",
+    originalPricePerDay: "₹14,999",
+    discountTag: "Offroad",
+    imageUrl: "/Images/toyota_hilux.avif"
+  },
+  {
+    id: 5,
     name: "MG Hector",
     type: "COMPACT SUV",
     pricePerDay: "₹6,499",
@@ -38,7 +47,7 @@ export const carsData: Car[] = [
     imageUrl: "/Images/mghector.avif",
   },
   {
-  id: 5,
+  id: 6,
     name: "Mahindra Thar",
     type: "Offroad SUV",
     pricePerDay: "₹5,499",
@@ -47,7 +56,16 @@ export const carsData: Car[] = [
     imageUrl: "/Images/thar.avif"
   },
   {
-    id: 6,
+    id: 7,
+    name: "Mahindra Thar Roxx 4WD",
+    type: "SUV",
+    pricePerDay: "₹7,999",
+    originalPricePerDay: "₹11,499",
+    discountTag: "Popular",
+    imageUrl: "/Images/roxx.avif"
+  },
+  {
+    id: 8,
     name: "MG Astor",
     type: "COMPACT SUV",
     pricePerDay: "₹4,499",
@@ -56,7 +74,7 @@ export const carsData: Car[] = [
     imageUrl: "/Images/astor.avif"
    },
   {
-    id: 7,
+    id: 9,
     name: "Xuv 500",
     type: "SUV",
     pricePerDay: "₹4,499",
@@ -65,7 +83,7 @@ export const carsData: Car[] = [
     imageUrl: "/Images/xuv500.jpg"  
   },
   {
-    id: 8,
+    id: 10,
     name: "Hyundai i20",
     type: "Hatchback",
     pricePerDay: "₹3,499",
@@ -74,7 +92,7 @@ export const carsData: Car[] = [
     imageUrl: "/Images/i20.jpg"
   },
   {
-    id: 9,
+    id: 11,
     name: "Maruti Swift",
     type: "SUV",
     pricePerDay: "₹3,499",
@@ -83,12 +101,12 @@ export const carsData: Car[] = [
     imageUrl: "/Images/swift.jpg"
   },
   {
-    id: 10,
-    name: "Cheverolet Aveo",
-    type: "Sedan",
-    pricePerDay: "₹2,499",
-    originalPricePerDay: "₹4,999",
-    discountTag: "50%",
-    imageUrl: "../../Images/aveo.avif"  
+    id: 12,
+    name: "Kia Seltos",
+    type: "Compact SUV",
+    pricePerDay: "₹4,999",
+    originalPricePerDay: "₹6,499",
+    discountTag: "Budget Pick",
+    imageUrl: "../../Images/seltos.avif"  
   }
 ];
